cc9badbfe199c63fe46f36c3002e9d48d9947c41
phyothantzin <phyothantzin.w@gmail.com>
2023-05-15 10:41:48 +0630

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>About</h1>
+  <p>
+    Velvet caught i nevermore nodded though let lenore lenore quoth, chamber i
+    hear it the i thereat nevermore. So tufted wheeled with beguiling, to morrow
+    there craven heard raven scarce before, that echo repeating this core this
+    is whether still. Silence crest nothing the thy.
+  </p>
+  <router-link to="/">Go back</router-link>
+</template>
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
